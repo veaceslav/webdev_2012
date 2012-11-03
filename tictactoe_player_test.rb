d20bfae5d956922b469740326e230f0f59c89664
Veaceslav Munteanu <slavuttici@gmail.com>
2012-11-03 12:43:08 +0000

    RED: test if turn accept only hash

--- tictactoe_player_test.rb
+++ tictactoe_player_test.rb
@@ -11,21 +11,19 @@ class TicTacToePlayer
                                 [:a1, :b2, :c3],
                                 [:c1, :b2, :a3]]
     def turn(move = {})
-        
+       
     end
 end
 
 class TestPlayer < Test::Unit::TestCase
-  def test_truthiness
-    functionality_implemented = true
-    assert( functionality_implemented, failure_message = "Functionality not implemented" )
-  end
 
   def test_respond_turn
 	assert(TicTacToePlayer.new.respond_to?("turn"), failure_mess = "Doesn't respond to turn")
-  end
+    puts "Test 1"
+   end
 
   def test_turn_hash
+    puts "Test 2"
     emptyhash = {"a"=>1}
     assert_nothing_raised(ArgumentError) do
         TicTacToePlayer.new.turn(emptyhash)
@@ -33,13 +31,14 @@ class TestPlayer < Test::Unit::TestCase
   end
 
   def test_turn_accept_2var_hash
-    secondhash = {:owned_by_x =>nil, :owned_by_zero => nil}
-    assert_nothing_raised(ArgumentError) do
-        TicTacToePlayer.new.turn(secondhash)
-    end
+        puts "Test 3"
+        val =1
+        assert(TicTacToePlayer.new.turn(val).equal?(1),failure_mess = "Turn accept non hash")
+
   end
 
   def test_turn_accept_hash_3
+    puts "Test 4"
     thirdhash = {:owned_by_x =>[], :owned_by_zero => []}
     assert_nothing_raised(ArgumentError) do
         TicTacToePlayer.new.turn(thirdhash)
