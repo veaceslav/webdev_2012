6cd234643cad4c3b9229c735e72cba31c92f0e00
Veaceslav Munteanu <slavuttici@gmail.com>
2012-11-03 11:44:28 +0000

    GREEN: turn method exist

--- tictactoe_player_test.rb
+++ tictactoe_player_test.rb
@@ -1,6 +1,18 @@
 require 'test/unit'
 
 class TicTacToePlayer
+    BOARD = [:a1,:a2, :a3, :b1, :b2, :b3, :c1, :c2, :c3]
+      WINNING_COMBINATIONS = [[:a1, :a2, :a3],
+                                [:b1, :b2, :b3],
+                                [:c1, :c2, :c3],
+                                [:a1, :b1, :c1],
+                                [:a2, :b2, :c2],
+                                [:a3, :b3, :c3],
+                                [:a1, :b2, :c3],
+                                [:c1, :b2, :a3]]
+    def turn()
+        
+    end
 end
 
 class TestPlayer < Test::Unit::TestCase
@@ -10,7 +22,7 @@ class TestPlayer < Test::Unit::TestCase
   end
 
   def test_respond_turn
-	assert(TicTacToePlayer.respond_to?("turn"), failure_mess = "Doesn't respond to turn");
+	assert(TicTacToePlayer.new.respond_to?("turn"), failure_mess = "Doesn't respond to turn");
   end
 end
 
