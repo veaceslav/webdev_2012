fabecbe3461034a8eaafe508cb59bd4240079fe8
Veaceslav Munteanu <slavuttici@gmail.com>
2012-11-03 11:23:31 +0000

    RED: turn method exist

--- tictactoe_player_test.rb
+++ tictactoe_player_test.rb
@@ -8,4 +8,10 @@ class TestPlayer < Test::Unit::TestCase
     functionality_implemented = true
     assert( functionality_implemented, failure_message = "Functionality not implemented" )
   end
+
+  def test_respond_turn
+	assert(TicTacToePlayer.respond_to?("turn"), failure_mess = "Doesn't respond to turn");
+  end
 end
+
+
