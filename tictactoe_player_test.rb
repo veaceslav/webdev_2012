62fb9daeb51ccc55fce539816fb33580aa373791
Veaceslav Munteanu <slavuttici@gmail.com>
2012-11-03 13:35:17 +0000

    RED: test nil value when non array

--- tictactoe_player_test.rb
+++ tictactoe_player_test.rb
@@ -12,12 +12,14 @@ class TicTacToePlayer
                                 [:c1, :b2, :a3]]
     def turn(move = {})
       if(!move.instance_of? Hash)
-          return 1;
+          return nil;
       end 
       
-      if(move.has_key?(:owned_by_x) && move.has_key?(:owned_by_zero))
-          return 2;
+      if(!move.has_key?(:owned_by_x) && !move.has_key?(:owned_by_zero))
+          return nil;
       end
+
+      return 1;
     end
 end
 
@@ -39,7 +41,7 @@ class TestPlayer < Test::Unit::TestCase
   def test_turn_accept_2var_hash
         puts "Test 3"
         val =1
-        assert(TicTacToePlayer.new.turn(val).equal?(1),failure_mess = "Turn accept non hash")
+        assert(TicTacToePlayer.new.turn(val).equal?(nil),failure_mess = "Turn accept non hash")
 
   end
 
@@ -54,7 +56,13 @@ class TestPlayer < Test::Unit::TestCase
   def test_turn_return_not_nil
     puts "Test 5"
     thirdhash = {:owned_by_x =>[], :owned_by_zero => []}
-    assert(TicTacToePlayer.new.turn(thirdhash).equal?(2),err_mess = "Turn doesn't return 2 when given right input")
+    assert(TicTacToePlayer.new.turn(thirdhash).equal?(1),err_mess = "Turn doesn't return 1 when given right input")
+  end
+
+  def test_turn_return_nil_on_non_array_values
+    puts "Test 6"
+    thirdhash = {:owned_by_x =>1, :owned_by_zero =>"abba"}
+    assert(TicTacToePlayer.new.turn(thirdhash).equal?(nil),err_mess = "Turn doesn't return nil when non array values in hash")
   end
 
 
